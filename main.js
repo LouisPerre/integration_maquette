9bfab5b2c08f908fce0b34b2b1f1e22a3f6ef878
Louis Perrenot <louis.perrenot@edu.devinci.fr>
2021-11-21 18:55:34 +0100

    :lipstick: CLEANING CSS

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ menuBurger.addEventListener("click", function() {
     menuBurgerContent.classList.toggle('open')
     //choice.classList.toggle('open')
 })
+
+window.addEventListener('resize', () =>{
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
